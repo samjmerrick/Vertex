0d3866dada1ac79675e2a24164337e2b8c31554a
Samuel Merrick <samjmerrick@gmail.com>
2018-04-03 17:44:57 +0100

    Added Facebook SDK, Fetches profile image

---
+++ Assets/Scripts/FbController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Facebook.Unity;
+using UnityEngine.UI;
+
+public class FbController : MonoBehaviour {
+
+	public RawImage PlayerImage;
+	List<string> perms = new List<string>(){"public_profile", "email"};
+
+	public void LogIn(){
+		FB.LogInWithReadPermissions(perms, AuthCallback);
+	}
+
+	// Awake function from Unity's MonoBehavior
+	void Awake ()
+	{
+		if (!FB.IsInitialized) {
+			// Initialize the Facebook SDK
+			FB.Init(InitCallback, OnHideUnity);
+		} else {
+			// Already initialized, signal an app activation App Event
+			FB.ActivateApp();
+		}
+	}
+
+	private void InitCallback ()
+	{
+		if (FB.IsInitialized) {
+			// Signal an app activation App Event
+			FB.ActivateApp();
+			// Continue with Facebook SDK
+			// ...
+		} else {
+			Debug.Log("Failed to Initialize the Facebook SDK");
+		}
+
+		if (FB.IsLoggedIn){
+			FB.API( "/me/picture?type=square&height=128&width=128", HttpMethod.GET, GetPicture);
+		}
+	}
+
+	private void OnHideUnity (bool isGameShown)
+	{
+		if (!isGameShown) {
+			// Pause the game - we will need to hide
+			Time.timeScale = 0;
+		} else {
+			// Resume the game - we're getting focus again
+			Time.timeScale = 1;
+		}
+	}
+
+	private void AuthCallback (ILoginResult result) {
+		if (FB.IsLoggedIn) {
+			// AccessToken class will have session details
+			var aToken = Facebook.Unity.AccessToken.CurrentAccessToken;
+			// Print current access token's User ID
+			Debug.Log(aToken.UserId);
+			// Print current access token's granted permissions
+			foreach (string perm in aToken.Permissions) {
+				Debug.Log(perm);
+			}
+		
+		} else {
+			Debug.Log("User cancelled login");
+		}
+	}
+	private void GetPicture(IGraphResult result){
+
+		Debug.Log("Getting profile picture..");
+		if (result.Error == null && result.Texture != null) {
+			Debug.Log("Found profile picture..");
+			PlayerImage.texture = result.Texture;
+		}
+
+	}
+}
