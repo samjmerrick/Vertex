9759bfd9ae51bd7cefb485cf6a7f163ce15a71b9
Sam Merrick <samjmerrick@gmail.com>
2018-04-07 11:12:43 +0100

    Drag anywhere to start game

---
+++ Assets/Scripts/Player/StartingShip.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class StartingShip : MonoBehaviour {
+
+    public GameObject Ship;
+
+    private void OnMouseDown()
+    {
+        if (!GameController.instance.GameRunning)
+        {
+            Instantiate(Ship, transform.position, transform.rotation);
+            GameController.instance.BeginGame();
+            Destroy(gameObject);
+        }     
+    }
+}
