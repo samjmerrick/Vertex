8327f7ab3cd0b046f027549b258671431a68a4b3
Sam Merrick <samjmerrick@gmail.com>
2018-04-10 17:15:11 +0100

    Fixed game starting from other menus

--- Assets/Scripts/Player/StartingShip.cs
+++ Assets/Scripts/Player/StartingShip.cs
@@ -3,10 +3,11 @@
 public class StartingShip : MonoBehaviour {
 
     public GameObject Ship;
+    public PanelManager panelManager;
 
     private void OnMouseDown()
     {
-        if (!GameController.instance.GameRunning)
+        if (!GameController.instance.GameRunning && panelManager.CurrentPanel.name == "Start Menu")
         {
             Instantiate(Ship, transform.position, transform.rotation);
             GameController.instance.BeginGame();
