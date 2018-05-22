8566029c55fed0ed87043f297741b312183a5673
Sam Merrick <samjmerrick@gmail.com>
2018-05-22 19:26:56 +0100

    Fixed touch anywhere to start game

--- Assets/Scripts/Player/Ship/StartingShip.cs
+++ Assets/Scripts/Player/Ship/StartingShip.cs
@@ -4,7 +4,20 @@ public class StartingShip : MonoBehaviour {
 
     public PanelManager panelManager;
 
-    private void OnMouseDown()
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            float diff = Vector2.Distance(mousePos, transform.position);
+
+            if (diff < 2.5f)
+                StartGame();
+
+        }
+    }
+
+    void StartGame()
     {
         if (!GameController.instance.GameRunning && panelManager.CurrentPanel.name == "Start Menu")
         {
@@ -12,6 +25,6 @@ public class StartingShip : MonoBehaviour {
 
             GetComponent<ShipMove>().enabled = true;
             enabled = false;
-        }     
+        }
     }
 }
