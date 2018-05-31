d512e053ffd14ddd7970c1dddca222f4f51e5dba
Sam Merrick <samjmerrick@gmail.com>
2018-05-31 19:24:54 +0100

    Initial firebase auth

--- Assets/Scripts/Social/FbController.cs
+++ Assets/Scripts/Social/FbController.cs
@@ -48,15 +48,44 @@ public class FbController : MonoBehaviour {
         }
 	}
 
-	private void AuthCallback (ILoginResult result)
+
+
+    private void AuthCallback (ILoginResult result)
     {
 		if (FB.IsLoggedIn)
         {
 			// AccessToken class will have session details
 			var aToken = Facebook.Unity.AccessToken.CurrentAccessToken;
 
-			// Print current access token's User ID
-			Debug.Log(aToken.UserId);
+            string accessToken = AccessToken.CurrentAccessToken.ToString();
+
+            Firebase.Auth.Credential credential =
+                Firebase.Auth.FacebookAuthProvider.GetCredential(accessToken);
+
+
+            Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+
+            auth.SignInWithCredentialAsync(credential).ContinueWith(task => {
+                if (task.IsCanceled)
+                {
+                    Debug.LogError("SignInWithCredentialAsync was canceled.");
+                    return;
+                }
+                if (task.IsFaulted)
+                {
+                    Debug.LogError("SignInWithCredentialAsync encountered an error: " + task.Exception);
+                    return;
+                }
+
+                Firebase.Auth.FirebaseUser newUser = task.Result;
+                Debug.LogFormat("User signed in successfully: {0} ({1})",
+                    newUser.DisplayName, newUser.UserId);
+            });
+
+
+
+            // Print current access token's User ID
+            Debug.Log(aToken.UserId);
 
 			// Print current access token's granted permissions
 			foreach (string perm in aToken.Permissions) {
