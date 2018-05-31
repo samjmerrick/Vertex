d1acffb0d8ea7f7f091b499e1c6f0cbfa1b641c4
Sam Merrick <samjmerrick@gmail.com>
2018-05-31 18:31:28 +0100

    Integrate Firebase Analytics

---
+++ Assets/FirebaseTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirebaseTest : MonoBehaviour {
+
+    private void Awake()
+    {
+        Firebase.Analytics.FirebaseAnalytics
+          .LogEvent(
+            Firebase.Analytics.FirebaseAnalytics.EventJoinGroup,
+            Firebase.Analytics.FirebaseAnalytics.ParameterGroupId,
+            "Hello World"
+          );
+    }
+}
