7ec8079b25b15d5b1717a3a80e2815edde342f6a
Sam Merrick <samjmerrick@gmail.com>
2018-06-08 10:16:25 +0100

    Check dependencies before using Firebase Auth

--- Assets/Scripts/Social/FirebaseUser.cs
+++ Assets/Scripts/Social/FirebaseUser.cs
@@ -6,6 +6,31 @@ public class FirebaseUser : MonoBehaviour {
 
     public static Firebase.Auth.FirebaseUser user;
 
+    private void Awake()
+    {
+        CheckDependencies();
+    }
+
+
+    void CheckDependencies()
+    {
+        // Code to check for Google play services - required for Android
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Set a flag here indiciating that Firebase is ready to use by your
+                // application.
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+    }
+
     public void SignInWithFacebook(string accessToken)
     {
         Firebase.Auth.Credential credential =
