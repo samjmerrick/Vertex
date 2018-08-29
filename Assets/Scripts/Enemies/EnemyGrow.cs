0cfb7770439583a464ad1eb6d2b1bb8da3c8fd75
Sam Merrick <samjmerrick@gmail.com>
2018-08-29 20:12:44 +0100

    Add growing red circle enemy

---
+++ Assets/Scripts/Enemies/EnemyGrow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGrow : MonoBehaviour {
+
+    public float Speed;
+    public Vector2 MinMax;
+
+	void Update ()
+    {
+        var range = MinMax.y - MinMax.x;
+
+        float scale = (Mathf.Sin(Time.time * Speed) + 1.0f) / 2.0f * range + MinMax.x;
+        transform.localScale = new Vector3(scale, scale);
+    }
+}
