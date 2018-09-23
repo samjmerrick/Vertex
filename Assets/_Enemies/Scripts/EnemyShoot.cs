5bf432fd0e680296e221a8c99075f3f7ceba5dcc
Sam Merrick <samjmerrick@gmail.com>
2018-09-23 13:47:44 +0100

    Added indication of shooting via shrink

--- Assets/_Enemies/Scripts/EnemyShoot.cs
+++ Assets/_Enemies/Scripts/EnemyShoot.cs
@@ -9,22 +9,52 @@ public class EnemyShoot : MonoBehaviour
     public float ShootSpeed;
     public float cooldown = 2;
     public int chanceToShoot;
-    
+
+    private bool isShrinking;
+    private float targetShrink;
+    private float shrinkSpeed = 0.8f;
+
     private void Start()
     {
         InvokeRepeating("Shoot", cooldown, cooldown);
+        targetShrink = transform.localScale.x - .1f;
     }
 
     void Shoot()
     {
         if (Random.Range(0, 10) < chanceToShoot)
         {
-            foreach (Transform point in shootPoints)
+            StartCoroutine(ShrinkAndShoot());
+        }
+    }
+
+    IEnumerator ShrinkAndShoot()
+    {
+        isShrinking = true;
+        yield return new WaitUntil(() => !isShrinking);
+
+        foreach (Transform point in shootPoints)
+        {
+            GameObject go = Instantiate(Bullet, point.position, point.rotation);
+            go.GetComponent<Rigidbody2D>().AddForce(-go.transform.up * ShootSpeed);
+        }
+    }
+
+    private void Update()
+    {
+        if (isShrinking)
+        {
+            transform.localScale -= Vector3.one * Time.deltaTime * shrinkSpeed;
+     
+
+            if (transform.localScale.x < targetShrink)
             {
-                GameObject go = Instantiate(Bullet, point.position, point.rotation);
-                go.GetComponent<Rigidbody2D>().AddForce(-go.transform.up * ShootSpeed);
+                transform.localScale = new Vector3(targetShrink + .1f, targetShrink + .1f);
+                isShrinking = false;
+            
             }
         }
+            
     }
 }
 
