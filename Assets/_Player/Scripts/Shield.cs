c6749e450b8e03ff20ec589fe441295960af0210
Sam Merrick <samjmerrick@gmail.com>
2018-10-07 13:14:57 +0100

    Disable ShipTakeDamage while shield is active

---
+++ Assets/_Player/Scripts/Shield.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour {
+
+    ShipTakeDamage sd;
+
+    private void OnEnable()
+    {
+        sd = GetComponentInParent<ShipTakeDamage>();
+
+        if (sd == null)
+        {
+            Debug.Log("Shield tried to disable ShipTakeDamage component, but none was found");
+            enabled = false;
+        }
+
+        sd.enabled = false;
+    }
+
+    private void OnDisable()
+    {
+        sd.enabled = true;
+    }
+}
