7cd79f580147ebf266564e219848d6ba6e9ff5e4
Sam Merrick <samjmerrick@gmail.com>
2018-10-09 22:17:20 +0100

    Import VectorGrid

---
+++ Assets/VectorGrid/Scripts/VectorGridScroll.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorGridScroll : MonoBehaviour {
+
+    public Vector2 m_ScrollSpeed;
+    VectorGrid m_VectorGrid;
+
+	// Use this for initialization
+	void Start () {
+        m_VectorGrid = GetComponent<VectorGrid>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_VectorGrid.Scroll(m_ScrollSpeed * Time.deltaTime);
+    }
+}
