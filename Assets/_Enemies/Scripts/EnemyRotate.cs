04313ed2bae0faeb2637e87d7c4c260f46bd846f
Sam Merrick <samjmerrick@gmail.com>
2018-11-20 19:44:38 +0000

    50% chance to be left or right rotation

--- Assets/_Enemies/Scripts/EnemyRotate.cs
+++ Assets/_Enemies/Scripts/EnemyRotate.cs
@@ -8,7 +8,9 @@ public class EnemyRotate : MonoBehaviour
     void Start()
     {
         float angularVelocity = Random.Range(AngularVelocity.x, AngularVelocity.y);
+
+        angularVelocity *= Random.Range(0, 2) * 2 - 1; // Random 1 or -1, 50% chance to be left or right rotation
+
         GetComponent<Rigidbody2D>().angularVelocity = angularVelocity;
     }
-
 }
