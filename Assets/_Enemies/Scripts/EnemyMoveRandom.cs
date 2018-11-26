97914939608df6232c6ed97e67a78a8c16c88945
Sam Merrick <samjmerrick@gmail.com>
2018-11-26 20:24:19 +0000

    Add EnemyMoveRandom

---
+++ Assets/_Enemies/Scripts/EnemyMoveRandom.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMoveRandom : MonoBehaviour {
+
+    public float Speed;
+
+	void Start ()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+        transform.rotation = Quaternion.Euler(0, 0, Random.Range(0, 360));
+
+        rb.AddForce(transform.up * Speed);
+	}
+}
