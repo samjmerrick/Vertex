72af9ca50f59da21ab6475729b367866346374c7
Sam Merrick <samjmerrick@gmail.com>
2018-12-01 11:40:39 +0000

    New enemy script: SplitOnDestroy

---
+++ Assets/_Enemies/Scripts/EnemySplitOnDestroy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySplitOnDestroy : MonoBehaviour {
+
+    public GameObject[] thingsToSpawn;
+
+    private void OnDestroy()
+    {
+        if (thingsToSpawn.Length > 0 && GameController.GameRunning)
+        {
+            float angle = 180 / thingsToSpawn.Length;
+            int ran = 0;
+
+            foreach (GameObject toSpawn in thingsToSpawn)
+            {
+                Instantiate(
+                    original: toSpawn,
+                    position: transform.position,
+                    rotation: Quaternion.Euler(0, 0, Random.Range(0, 360))
+                    );
+
+                ran++;
+            }
+        }
+    }
+}
