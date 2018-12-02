51144153dc79bad95d7a4d8951095c4b020441ef
Sam Merrick <samjmerrick@gmail.com>
2018-12-02 16:18:15 +0000

    Added VectorGridExplosion for particle effects

---
+++ Assets/VectorGrid/Scripts/VectorGridExplosion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorGridExplosion : MonoBehaviour {
+
+    public float m_ForceScale = 1;
+    public float m_Radius = 1;
+    public float duration = 0.4f;
+
+    private VectorGrid m_VectorGrid;
+    private Color m_Color;
+
+	IEnumerator Start ()
+    {
+        m_VectorGrid = FindObjectOfType<VectorGrid>();
+        m_Color = GetComponent<ParticleSystem>().main.startColor.color;
+
+        yield return new WaitForSeconds(duration);
+        enabled = false;
+    }
+
+	void Update ()
+    {
+        m_VectorGrid.AddGridForce(this.transform.position, m_ForceScale, m_Radius, m_Color, true);
+    }
+}
--- Assets/VectorGrid/Scripts/VectorGridForce.cs
+++ Assets/VectorGrid/Scripts/VectorGridForce.cs
@@ -11,14 +11,6 @@ public class VectorGridForce : MonoBehaviour
 	public Color m_Color = Color.white;
 	public bool m_HasColor;
 
-    public bool m_findVectorGrid;
-
-    private void Start()
-    {
-        if (m_findVectorGrid)
-            m_VectorGrid = FindObjectOfType<VectorGrid>();
-    }
-
     // Update is called once per frame
     void Update () 
 	{
