d4364653a82fae90502571a1e6d341c956c7cd6d
Sam Merrick <samjmerrick@gmail.com>
2018-12-07 19:23:26 +0000

    Added SFX and temp music

---
+++ Assets/_SFX/AudioManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    private static AudioSource Source;
+
+    private void Start()
+    {
+        Source = GetComponent<AudioSource>();
+    }
+
+    public static void PlaySound(AudioClip Clip)
+    {
+        Source.clip = Clip;
+        Source.Play();
+    }
+}
