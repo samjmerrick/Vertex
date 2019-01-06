b255b6fa1813920fcde758436ce291dca3f51e49
Samuel Merrick <samjmerrick@gmail.com>
2019-01-06 08:34:41 +0000

    Added DestroyOnNoChildren for enemy groups

---
+++ Assets/_Enemies/Scripts/DestroyOnNoChildren.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnNoChildren : MonoBehaviour {
+
+    private void OnTransformChildrenChanged()
+    {
+        if (transform.childCount == 0)
+            Destroy(gameObject);
+    }
+}
