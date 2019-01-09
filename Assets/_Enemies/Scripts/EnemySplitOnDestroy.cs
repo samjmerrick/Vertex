2cf77fcc215a828fefb5d8324f14d4ff2a8aae12
Sam Merrick <samjmerrick@gmail.com>
2019-01-09 12:50:39 +0000

    Only split when PlayerFire hits

--- Assets/_Enemies/Scripts/EnemySplitOnDestroy.cs
+++ Assets/_Enemies/Scripts/EnemySplitOnDestroy.cs
@@ -7,32 +7,19 @@ public class EnemySplitOnDestroy : MonoBehaviour {
     public GameObject objectToSpawn;
     public int numberToSpawn;
 
-    private Vector2 bounds;
-    private bool isQuitting = false;
-
-    private void OnDestroy()
+    private void OnTriggerEnter2D(Collider2D c)
     {
-        // Check there is an object to spawn, and the game is running
-        if (objectToSpawn != null && GameController.GameRunning && !isQuitting)
+        // Check collision is with PlayerFire and our health is 0
+        if (c.gameObject.tag == "PlayerFire" && GetComponent<Enemy>().Health == 0)
         {
-            // Check we're above the bottom of the screen + 1
-            bounds = Bounds.Get();
-            if (transform.position.y > -bounds.y + 1)
+            for (int i = 0; i < numberToSpawn; i++)
             {
-                for (int i = 0; i < numberToSpawn; i++)
-                {
-                    Instantiate(
-                        original: objectToSpawn,
-                        position: transform.position,
-                        rotation: Quaternion.Euler(0, 0, Random.Range(0, 360))
-                        );
-                }
-            }
+                Instantiate(
+                    original: objectToSpawn,
+                    position: transform.position,
+                    rotation: Quaternion.Euler(0, 0, Random.Range(0, 360))
+                    );
+            } 
         }
-    }
-
-    private void OnApplicationQuit()
-    {
-        isQuitting = true;
     }
 }
