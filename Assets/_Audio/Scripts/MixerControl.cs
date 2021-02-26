381a4faca45bae26fb31c1d112e892f376fcd766
Sam Merrick <samjmerrick@gmail.com>
2021-02-26 06:15:29 +0000

    Audio folder restructure

--- Assets/_Audio/Scripts/MixerControl.cs
+++ Assets/_Audio/Scripts/MixerControl.cs
@@ -4,7 +4,8 @@ using UnityEngine.UI;
 
 public class MixerControl : MonoBehaviour
 {
-    public AudioMixer mixer;
+    public AudioMixer MusicMixer;
+    public AudioMixer SFXMixer;
 
     private string musicPref = "Music";
     private string sfxPref = "SFX";
@@ -15,20 +16,24 @@ public class MixerControl : MonoBehaviour
     void Start()
     {
         float savedMusicVolume = PlayerPrefs.GetFloat(musicPref, 1);
-        mixer.SetFloat(musicMixerParam, convertToLinear(savedMusicVolume));
+        MusicMixer.SetFloat(musicMixerParam, convertToLinear(savedMusicVolume));
 
         float savedSFXVolume = PlayerPrefs.GetFloat(sfxPref, 1);
-        mixer.SetFloat(sfxMixerParam, convertToLinear(savedSFXVolume));
+        SFXMixer.SetFloat(sfxMixerParam, convertToLinear(savedSFXVolume));
     }
 
     public void SetMusicLevel(float volume)
     {
-        SetLevel(musicMixerParam, musicPref, volume);
+        // Save the value before conversion to linear
+        PlayerPrefs.SetFloat(musicPref, volume);
+        MusicMixer.SetFloat(musicMixerParam, convertToLinear(volume));
     }
 
     public void SetSFXLevel(float volume)
     {
-        SetLevel(sfxMixerParam, sfxPref, volume);
+        // Save the value before conversion to linear
+        PlayerPrefs.SetFloat(sfxPref, volume);
+        SFXMixer.SetFloat(sfxMixerParam, convertToLinear(volume));
     }
 
     public float ReturnMusicLevel()
@@ -41,17 +46,8 @@ public class MixerControl : MonoBehaviour
         return PlayerPrefs.GetFloat(sfxPref, 1);
     }
 
-    private void SetLevel(string mixerParam, string playerPref, float volume)
-    {
-        // Save the value before conversion to linear
-        PlayerPrefs.SetFloat(playerPref, volume);
-
-        mixer.SetFloat(mixerParam, convertToLinear(volume));
-    }
-
     private float convertToLinear(float value)
     {
         return Mathf.Log10(value) * 20;
     }
-
 }
